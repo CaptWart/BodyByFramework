34eca93a6a6a9a3ca04462e51c0742664bfe0f58
Emi Clar <kotaki@gmail.com>
2020-05-16 10:20:39 -0400

    Fixed add and delete day functionalities.

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -1,47 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
-import API from "../Utils/API";
 
 function Day(props) {
-
-  const [lastDay, setLastDay] = useState({_id: 0, day: 0});
-
-  useEffect(() => {
-    console.log("this is the current selectedPlan: ", props.selectedPlan);
-    loadLastDay(props.selectedPlan);
-  }, [props.selectedPlan]);
-
-  const loadLastDay = planID => {
-    API.getLastDay(planID)
-    .then(res => {
-      console.log("res.data: ", res.data);
- 
-      setLastDay(res.data);
-    })
-    .catch(err =>
-      console.log(err)
-    );
-  }
-
   return (
     <div>
       <h3>{props.days.length} Days Plan</h3>
 
-        <Button
-          name="addBtn"
-          variant="primary" 
-          type="submit" 
-          value={lastDay.length > 0 ? lastDay[0].day : 0}
-          onClick={props.handleSaveDay}
-        >
-          Add Day
-        </Button>
+      <Button
+        name="addBtn"
+        variant="primary" 
+        type="submit" 
+        onClick={props.handleSaveDay}
+      >
+        Add Day
+      </Button>
+
       {props.days.length > 0 ?
         <Button
           name="deleteBtn"
           variant="danger" 
           type="submit"
-          value={lastDay.length > 0 ? lastDay[0]._id : 0}
           onClick={props.handleDeleteDay}
         >
           Remove Day
